52fdd53c79bee1cef4820e67cfc4971f56702126
HarryZhou <2373256746@qq.com>
2022-07-27 19:57:56 +0800

    __

---
+++ test/testground/testEigen.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "../../Eigen/Dense"
+
+void testF(Eigen::Ref<Eigen::MatrixXd> a)
+{
+    std::cout << "Ref Receive\n"
+              << a << std::endl;
+}
+
+int main()
+{
+    Eigen::MatrixXd a{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    auto b = (a.array() * -2).matrix();
+    auto d = a.array().max(b.array());
+    std::cout << "a = \n"
+              << a << std::endl;
+    std::cout << "a.array().inverse() = \n"
+              << a.array().max(b.array()) << std::endl;
+    // auto aa = Eigen::MatrixXd::Random(3,3);
+    // testF(aa);
+
+    return 0;
+}
